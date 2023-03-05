5eb364e81da8d6b01339c8f71ae2aa54cb4f5a37
您的姓名 <Email>
2023-03-05 14:23:13 +0800

    git init

---
+++ src/components/TestModal.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>產品modal頁</h1>
+  <label for="title" class="form-label">標題</label>
+                  <input id="title" type="text" class="form-control" placeholder="請輸入標題" v-text="template.abc">
+  <h3 >{{this.template.abc}}</h3>
+  <img class="img-fluid" :src="this.template.imagesUrl" alt="">
+</template>
+
+<script>
+export default {
+  props: ['template']
+}
+</script>
